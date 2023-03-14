99a23b88022e1af799adc82954cafd98e3c1b18f
AbuuAish <ayuba.shakiru@gmail.com>
2023-03-14 03:08:52 -0700

    Tasks on Lists

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 
 def no_c(my_string):
     updated_str = ''
-        for i in my_string:
-            if i != 'c' and i != 'C':
-                updated_str += i
-                return (updated_str)
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            updated_str += i
+            return (updated_str)
